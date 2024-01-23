4809c55fd08496aa307988b35276eba2ed1f868d
M1r1c1e <andersonmiracle111@.com>
2024-01-23 11:47:48 -0800

    using insertion sorting algorithm to sort list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,55 +2,44 @@
 #include <stdio.h>
 
 /**
- * insertion_sort_list - Sorting a doubly linked list
- * of integers in ascending order with Insertion sort algorithm.
+ * insertion_sort_list - Sorting a doubly linked list of integers in ascending order
+ *                       using the insertion sort algorithm.
  *
  * @list: Pointing to the doubly linked list head
  */
-void insertion_sort_list(listint_t **list);
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iterate_node = (*list)->next;
+	/* Initialization of variables */
+	listint_t *iterated_node, *moving_node;
 
-	/* is empty or has one or two nodes; if condition is true */
-	/* exit program because sorting will not be needed */
+	iterated_node = (*list)->next;
+
+	/* If list has one or two elements or is empty, exit the program */
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	/* initializing pointers: iterating node iterates*/
-	/* from second node through end of list */
-	/* moving node is the node that needs to be moved for sorting to take place */
-
-	while (iterate_node != NULL)
+	while (iterated_node != NULL)
 	{
-		listint_t *moving_node = iterate_node;
-
-		/* sorting process */
+		moving_node = iterated_node;
 		while (moving_node->prev != NULL && moving_node->n < moving_node->prev->n)
 		{
-			listint_t *prev_node = moving_node->prev;
-			listint_t *next_node = moving_node->next;
+			/* Swapping the nodes */
+			if (moving_node->next != NULL)
+				moving_node->next->prev = moving_node->prev;
+			moving_node->prev->next = moving_node->next;
+			moving_node->next = moving_node->prev;
+			moving_node->prev = moving_node->prev->prev;
+			moving_node->next->prev = moving_node;
 
-			/* swapping nodes */
-			if (prev_node != NULL)
-				prev_node->next = next_node;
+			if (moving_node->prev != NULL)
+				moving_node->prev->next = moving_node;
 			else
-				*list = next_node;
+				*list = moving_node;
 
-			if (next_node != NULL)
-				next_node->prev = prev_node;
-
-			moving_node->next = prev_node;
-			moving_node->prev = prev_node != NULL ? prev_node->prev : NULL;
-
-			if (prev_node != NULL)
-				prev_node->prev = moving_node;
-
-			/* Print the moving node after swapping */
+			/* Printing the list of swapped iterated node */
 			print_list(*list);
 		}
-
-		iterate_node = iterate_node->next;
+		iterated_node = iterated_node->next;
 	}
 }
 
